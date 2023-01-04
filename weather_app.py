a5e16fdd94cc1870675e13e273959716b6b1e546
Dan Li <danmuxingli@gmail.com>
2023-01-04 18:23:49 -0500

    get api key

--- weather_app.py
+++ weather_app.py
@@ -6,6 +6,12 @@ from datetime import datetime
 # root window
 root = Tk()
 root.config(bg="#3a3697", padx=10, pady=10)
+root.iconbitmap(r"C:\Users\danmu\my_projects\simple-weather-app\weather_images\wapp_icon.ico")
+
+# get api key from https://openweathermap.org/
+api_key = "4ae7fb8539618d265f7229f3d7431a45"
+
+
 
 # run
 root.mainloop()
