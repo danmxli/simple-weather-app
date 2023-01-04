a22da64a5e7a391c497e495919047271856f1db4
Dan Li <danmuxingli@gmail.com>
2023-01-04 12:10:47 -0500

    create root window of app

---
+++ weather_app.py
@@ -0,0 +1,13 @@
+# import
+from tkinter import *
+import sqlite3
+
+# root window
+root = Tk()
+root.title("Weather")
+root.iconbitmap(r"C:\Users\danmu\my_projects\simple-weather-app\weather_images\wapp_icon.ico")
+root['background']='#c3c1f7'
+root.config(padx=15)
+
+# run
+root.mainloop()
