17122e62f46236e3a54c0ec20b4f2430778d06e2
Dan Li <danmuxingli@gmail.com>
2023-01-03 23:19:27 -0500

    add custome icon and frame

---
+++ weather_app.py
@@ -0,0 +1,20 @@
+# import tkinter library
+from tkinter import *
+
+# create root window
+root = Tk()
+root.title('weather app testing gui')
+
+# custom icon
+root.iconbitmap(r"C:\Users\danmu\my_projects\simple-weather-app\weather_images\wapp_icon.ico")
+
+# frame
+main_frame = Frame(root, bg="#6eeac1", height=10, width=10)
+main_frame.pack()
+
+
+
+# label
+
+# run 
+root.mainloop()
