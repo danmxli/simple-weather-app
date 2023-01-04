a22be61e9552e321bf185839482ec7da78032888
Dan Li <danmuxingli@gmail.com>
2023-01-04 14:03:07 -0500

    fix root window after unexpected git merge conflict

--- weather_app.py
+++ weather_app.py
@@ -1,3 +1,11 @@
+from tkinter import *
+import requests
+import json
+from datetime import datetime
+
+# root window
+root = Tk()
+root.config(bg="#3a3697", padx=10, pady=10)
 
 # run
 root.mainloop()
