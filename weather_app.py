4b13b4f451421b182f743930cc26dec7f153e7a7
Dan Li <danmuxingli@gmail.com>
2023-01-05 15:42:25 -0500

    display data from dictionary to terminal

--- weather_app.py
+++ weather_app.py
@@ -8,22 +8,42 @@ root = Tk()
 root.config(bg="#3a3697", padx=10, pady=10)
 root.iconbitmap(r"C:\Users\danmu\my_projects\simple-weather-app\weather_images\wapp_icon.ico")
 
+
 # get api key from https://openweathermap.org/
 
 # api request function
 def fetch():
     api_key = "4ae7fb8539618d265f7229f3d7431a45"
     city_name = userin_entry.get()
-    userin_entry.delete(0, END)
+
     # request try and catch
     # api call from https://openweathermap.org/current
     try:
-        data = requests.get(
+        data_request = requests.get(
             f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&APPID={api_key}")
+        data = json.loads(data_request.content)
     except Exception as e:
-        print("request error")
-    # display json test
-    userin_entry.insert(0, data.json())
+        print("error")
+
+    # location
+    print(data['coord']['lon'])
+    print(data['coord']['lat'])
+
+    # details
+    print(data['weather'][0]['description'])
+    print(data['main']['temp'])
+    # advanced
+    print(data['main']['feels_like'])
+    print(data['main']['temp_min'])
+    print(data['main']['temp_max'])
+    print(data['main']['humidity'])
+    print(data['wind']['speed'])
+    print(data['wind']['deg'])
+
+
+    # read and insert
+    # userin_entry.insert(0)
+
 
 # entry
 userin_entry = Entry(root, width=20, borderwidth=10, font='arial 15')
@@ -34,7 +54,7 @@ userin_label = Label(root, text="Enter City")
 userin_label.pack()
 
 # buttons
-fetch_btn = Button(root, text="Get Data", command=lambda : fetch())
+fetch_btn = Button(root, text="Get Data", command=lambda: fetch())
 fetch_btn.pack()
 
 # run
