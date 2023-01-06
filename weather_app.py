9bc2891ef05c2673b35474e7de2e6c6a86a1922f
Dan Li <danmuxingli@gmail.com>
2023-01-06 03:18:15 -0500

    add timestamp for each request, add to database button on root window

--- weather_app.py
+++ weather_app.py
@@ -54,6 +54,7 @@ def fetch(type):
         return
 
     # define str variables
+    timestamp = str(datetime.now())
     lon = str(data['coord']['lon'])
     lat = str(data['coord']['lat'])
     weather_dsc = data['weather'][0]['description']
@@ -68,21 +69,22 @@ def fetch(type):
 
     # insert display
     display_w_info.delete("1.0", tkinter.END)
+    display_w_info.insert("1.0", "data retreived " + timestamp + "\n")
     if type == "s":
-        display_w_info.insert("3.0", "now: " + weather_dsc + "\n")
-        display_w_info.insert("4.0", "temperature: " + temperature + "\n")
+        display_w_info.insert("2.0", "now: " + weather_dsc + "\n")
+        display_w_info.insert("3.0", "temperature: " + temperature + "\n")
     if type == "d":
-        display_w_info.insert("1.0", "country: " + of_country + "\n")
-        display_w_info.insert("2.0", "longitude: " + lon + "\n")
-        display_w_info.insert("3.0", "latitude: " + lat + "\n")
-        display_w_info.insert("4.0", "now: " + weather_dsc + "\n")
-        display_w_info.insert("5.0", "temperature: " + temperature + "\n")
-        display_w_info.insert("6.0", "feels like: " + feels_like + "\n")
-        display_w_info.insert("7.0", "min. temperature: " + temp_min + "\n")
-        display_w_info.insert("8.0", "max. temperature: " + temp_max + "\n")
-        display_w_info.insert("9.0", "humidity: " + humidity + "\n")
-        display_w_info.insert("10.0", "wind speed: " + wind_speed + " m/s\n")
-        display_w_info.insert("11.0", "wind direction: " + wind_direction)
+        display_w_info.insert("2.0", "country: " + of_country + "\n")
+        display_w_info.insert("3.0", "longitude: " + lon + "\n")
+        display_w_info.insert("4.0", "latitude: " + lat + "\n")
+        display_w_info.insert("5.0", "now: " + weather_dsc + "\n")
+        display_w_info.insert("6.0", "temperature: " + temperature + "\n")
+        display_w_info.insert("7.0", "feels like: " + feels_like + "\n")
+        display_w_info.insert("8.0", "min. temperature: " + temp_min + "\n")
+        display_w_info.insert("9.0", "max. temperature: " + temp_max + "\n")
+        display_w_info.insert("10.0", "humidity: " + humidity + "\n")
+        display_w_info.insert("11.0", "wind speed: " + wind_speed + " m/s\n")
+        display_w_info.insert("12.0", "wind direction: " + wind_direction)
 
 
 
@@ -105,9 +107,12 @@ userin_entry.grid(row=1, column=0, padx=100)
 # buttons
 fetch_btn = Button(root, text="Get Data", borderwidth=5, command=lambda: fetch(view_type.get()))
 fetch_btn.grid(row=2, column=0)
-# button to access database
+# add to database
+addto_database_btn = Button(root, text="Add to Database", borderwidth=5)
+addto_database_btn.grid(row=4, column=0, pady=5)
+# access database
 open_database_btn = Button(root, text="Open Database", borderwidth=5, command=open_db_window)
-open_database_btn.grid(row=4, column=0)
+open_database_btn.grid(row=5, column=0, pady=5)
 
 """/// IN DEVELOPMENT ///"""
 # radiobutton
